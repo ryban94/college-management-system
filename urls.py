72f081dd5850db8de1aae1f32678852f8c0f64e0
ryban <ryban1@gmail.com>
2023-10-17 17:07:37 +0530

    login created

--- urls.py
+++ urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('stud_profile/',views.stud_profile,name="stud_profile"),
     path('stud_edit/',views.stud_edit,name="stud_edit"),
     path('stud_save/',views.stud_save,name="stud_save"),
+    path('stud_notification/',views.stud_notification,name="stud_notification"),
+    path('stud_user/',views.stud_user,name="stud_user"),
+    path('stud_login/',views.stud_login,name="stud_login")
 
 
 ]
--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from AdminUI.models import StudentDB,DepartmentDB,CourseDB,EnrollmentDB
+from AdminUI.models import StudentDB,DepartmentDB,CourseDB
 from django.core.files.storage import FileSystemStorage
 from django.utils.datastructures import MultiValueDictKeyError
 from django.contrib.auth import authenticate,login
@@ -13,15 +13,13 @@ def stud_profile(request):
     data1 = StudentDB.objects.all()
     data2 = DepartmentDB.objects.all()
     data3 = CourseDB.objects.all()
-    data4 = EnrollmentDB.objects.all()
-    return render(request,"student_profile.html",{'data1':data1,'data2':data2,'data3':data3,'data4':data4})
+    return render(request,"student_profile.html",{'data1':data1,'data2':data2,'data3':data3})
 
 def stud_edit(request):
     data1 = StudentDB.objects.all()
     data2 = DepartmentDB.objects.all()
     data3 = CourseDB.objects.all()
-    data4 = EnrollmentDB.objects.all()
-    return render(request,"student_edit.html",{'data1':data1,'data2':data2,'data3':data3,'data4':data4})
+    return render(request,"student_edit.html",{'data1':data1,'data2':data2,'data3':data3})
 
 def stud_save(request):
     if request.method=="POST":
@@ -34,6 +32,7 @@ def stud_save(request):
         stud_mob = request.POST.get('ContactNo')
         stud_address = request.POST.get('Address')
         stud_gcontact = request.POST.get('GuardianContact')
+        stud_gname = request.POST.get('GuardianName')
         stud_dept = request.POST.get('DeptName')
         stud_course = request.POST.get('CourseName')
         try:
@@ -46,17 +45,44 @@ def stud_save(request):
             StudentDB.objects.filter(StudentId=stud_id).update(FirstName=stud_fname,LastName=stud_lname,
                                                                DateOfBirth=stud_dob,Gender=stud_gender,
                                                                Email=stud_email,ContactNo=stud_mob,Address=stud_address,
-                                                               GuardianContact=stud_gcontact,DeptName=stud_dept,
+                                                               GuardianContact=stud_gcontact,GuardianName=stud_gname,DeptName=stud_dept,
                                                                CourseName=stud_course,Image=file)
         else:
             stud_image = request.Files['Image']
             obj=StudentDB(FirstName=stud_fname,LastName=stud_lname,
                           DateOfBirth=stud_dob,Gender=stud_gender,
                           Email=stud_email,ContactNo=stud_mob,Address=stud_address,
-                          GuardianContact=stud_gcontact,DeptName=stud_dept,
+                          GuardianContact=stud_gcontact,GuardianName=stud_gname,DeptName=stud_dept,
                           CourseName=stud_course,Image=stud_image)
             obj.save()
         return redirect(stud_profile)
+
+def stud_notification(request):
+    return render(request,'notification.html')
+
+def stud_user(request):
+    return render(request,'student_login.html')
+
+def stud_login(request):
+    if request.method=="POST":
+        stud_id = request.POST.get('user_id')
+        stud_pwd = request.POST.get('user_pwd')
+        if StudentDB.objects.filter(StudentId=stud_id,ContactNo=stud_pwd).exists():
+            request.session['StudentId']=stud_id
+            request.session['ContactNo']=stud_pwd
+            messages.success(request,"Login successfully")
+            return redirect(stud_profile)
+        else:
+            messages.error(request, "Invalid ID or Password")
+            return  redirect(stud_user)
+    else:
+        messages.error(request,"Invalid ID or Password")
+        return redirect(stud_user)
+
+def stud_logout(request):
+    del request.session['StudentId']
+    del request.session['ContactNo']
+
 
 
 
